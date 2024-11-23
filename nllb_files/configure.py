805a973d082decba6e0c2aeaa53bf78dfe9b4417
Carter Louchheim <carter.louchheim@gmail.com>
2024-11-22 20:47:43 -0500

    commitng to pull marks code

--- nllb_files/configure.py
+++ nllb_files/configure.py
@@ -1,7 +1,7 @@
 USE_CUDA=True
 
-LOG_FILE = 'model_logs.csv'
-TRAINING_NOTES = 'testing <gen> addition for quy monolingual corpora'
+LOG_FILE = 'results/model_logs.csv'
+TRAINING_NOTES = 'encrypted english (gne_Test) gen test, with <gen> encrypted monolingual addition'
 
 # hñähñu (worst performing lang)
 
@@ -18,7 +18,8 @@ AMERICAS_NLP_CODE_TO_LANG = {
     "tar": "raramuri",
     "hch": "wixarika",
     "eng": "english",
-    "spa": "spanish"
+    "spa": "spanish",
+    "gne": "encypted english"
 }
 
 NLLB_SEED_CSV = '/mnt/storage/hopkins/data/nllb/seed/nllb_seed.csv'
@@ -30,10 +31,11 @@ NLLB_SEED_LANGS = ['pbt_Arab', 'bho_Deva', 'nus_Latn', 'ban_Latn', 'dzo_Tibt', '
                    'taq_Tfng', 'shn_Mymr', 'mag_Deva']
 NLLB_SEED_LPS = [(src, 'eng_Latn') for src in NLLB_SEED_LANGS if src != 'eng_Latn']
 
-AMERICAS_NLP_CSV = '/mnt/storage/clouchheim/thesis-clouchheim/nllb_files/americas_nlp_data.csv'
+AMERICAS_NLP_CSV = '/mnt/storage/clouchheim/thesis-clouchheim/nllb_files/data/americas_nlp_data.csv'
 
 # For multi lingual training all 11
-ALL_AMERICAS_NLP_LANGS = ["cni_Latn", "bzd_Latn", "grn_Latn", "quy_Latn", "aym_Latn", "shp_Latn",
+
+ALL_LANGS = ["cni_Latn", "bzd_Latn", "grn_Latn", "quy_Latn", "aym_Latn", "shp_Latn",
                       "ctp_Latn", "oto_Latn", "nah_Latn", "tar_Latn", "hch_Latn"]
 
 
@@ -41,6 +43,6 @@ ALL_AMERICAS_NLP_LANGS = ["cni_Latn", "bzd_Latn", "grn_Latn", "quy_Latn", "aym_L
                       #"ctp_Latn", "oto_Latn", "nah_Latn", "tar_Latn", "hch_Latn"]
 
 # For bilingual or more language training
-AMERICAS_NLP_LANGS = ['quy_Latn'] # REMEMBER TO SET DEV LANAGUAGE IN RUN.SH
+AMERICAS_NLP_LANGS = ['gne_Test'] # REMEMBER TO SET DEV LANAGUAGE IN RUN.SH
 AMERICAS_NLP_LPS = [('spa_Latn', tgt) for tgt in AMERICAS_NLP_LANGS]
 
